4894b1bf28a06625e49e9d1c8aba060502c985bd
B0b5y <112172957+B0b5y@users.noreply.github.com>
2023-12-20 12:06:18 +0100

    add hometaskt razor

--- HomeTaskOrganizer/Models/HomeTask.cs
+++ HomeTaskOrganizer/Models/HomeTask.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Deadline { get; set; }
-        public string AsingetTo {  get; set; }
+        public string AsignetTo {  get; set; }
         public bool IsCompleted { get; set; }
 
     }
